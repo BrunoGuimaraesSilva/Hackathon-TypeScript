c38146e97dff4c82d7f7cbde0ed2a81f7f785fe5
Bruno Silva <bruno.sil16441@gmail.com>
2022-06-20 22:06:37 -0300

    feat: add login page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,44 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Image,
+} from "@chakra-ui/react";
+
+export default function Login() {
+  return (
+    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+      <Flex p={8} flex={1} align={"center"} justify={"center"}>
+        <Stack spacing={4} w={"full"} maxW={"md"}>
+          <Heading fontSize={"2xl"}>Login</Heading>
+          <FormControl id="email">
+            <FormLabel>E-mail</FormLabel>
+            <Input type="email" />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Senha</FormLabel>
+            <Input type="password" />
+          </FormControl>
+          <Stack spacing={6}>
+            <Button colorScheme={"blue"} variant={"solid"}>
+              Entrar
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={"Login Image"}
+          objectFit={"cover"}
+          src={
+            "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1115&q=80"
+          }
+        />
+      </Flex>
+    </Stack>
+  );
+}
